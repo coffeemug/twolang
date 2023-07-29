6470ff53cce69312e5217eb35d50f2f05192d553
Vyacheslav Akhmechet <coffeemug@gmail.com>
2023-07-29 15:01:15 -0700

    repl shell

--- cmd/repl.lisp
+++ cmd/repl.lisp
@@ -1,10 +1,15 @@
 (defpackage :twolang/cmd/repl
   (:use :cl)
   (:import-from :clingon)
+  (:import-from :cl-interpol)
+  (:import-from :cl-readline)
+  (:import-from :cl-ansi-text :with-color)
   (:export
    #:make-repl-command))
 
 (in-package :twolang/cmd/repl)
+
+(named-readtables:in-readtable :interpol-syntax)
 
 ;; clingon command stuff
 (defun make-repl-command ()
@@ -14,5 +19,57 @@
    :handler #'repl-command-handler))
 
 (defun repl-command-handler (cmd)
-  (declare (ignore cmd)))
+  (declare (ignore cmd))
+  (print-platform-header)
+  (maybe-start-slynk)
+  (repl))
 
+
+(defun print-platform-header ()
+  (let ((mt (machine-type))
+	(mv (machine-version))
+	(st (software-type))
+	(sv (software-version))
+	(lt (lisp-implementation-type))
+	(lv (lisp-implementation-version)))
+    (with-color (:black :effect :bright)
+      (format t #?"[${mt}/${mv}] [${st}/${sv}] [${lt}/${lv}] [Onelang/0.1]~%"))
+    (finish-output)))
+
+(defun maybe-start-slynk ()
+  "Dynamically load and start slynk server if the `slynk` system is
+installed."
+  (when (asdf:find-system :slynk nil)
+    (let* ((null-stream (make-two-way-stream
+			 (make-concatenated-stream)
+			 (make-broadcast-stream)))
+	   (*debug-io* null-stream))
+      (asdf:load-system :slynk))
+    (funcall
+     (find-symbol
+      (string '#:create-server)
+      (find-package :slynk)))))
+
+(defun repl ()
+  (let ((count 0))
+    (loop for line = (progn
+		       (finish-output)
+		       (readline (make-prompt count)))
+	  while line do (when (not (string= line ""))
+			  (eval-line line)
+			  (incf count)))))
+
+(defun readline (prompt)
+  (rl:readline :prompt prompt :add-history t))
+
+(defun make-prompt (count)
+  (with-output-to-string (stream)
+    (with-color (:green :stream stream)
+      (format stream "[" count))
+    (with-color (:green :effect :bright :stream stream)
+      (format stream "~d" count))
+    (with-color (:green :stream stream)
+      (format stream "]: " count))))
+
+(defun eval-line (line)
+  (declare (ignore line)))
