291da3a92abe5eab4ac92f60befeb0038030af76
Vyacheslav Akhmechet <coffeemug@gmail.com>
2023-07-30 11:09:57 -0700

    basic repl ctrl

--- cmd/repl.lisp
+++ cmd/repl.lisp
@@ -18,11 +18,18 @@
    :description "start twolang repl"
    :handler #'repl-command-handler))
 
+(defstruct repl-opts
+  (print-lex-p nil)
+  (print-ast-p nil)
+  (print-lisp-p nil))
+
 (defun repl-command-handler (cmd)
   (declare (ignore cmd))
   (print-platform-header)
   (maybe-start-slynk)
-  (repl))
+  (let ((*repl-ctrl* (make-repl-opts)))
+    (declare (special *repl-ctrl*))
+    (repl)))
 
 (defun print-platform-header ()
   (let ((mt (machine-type))
@@ -32,7 +39,7 @@
 	(lt (lisp-implementation-type))
 	(lv (lisp-implementation-version)))
     (with-color (:black :effect :bright)
-      (format t #?"[${mt}/${mv}] [${st}/${sv}] [${lt}/${lv}] [Onelang/0.1]~%"))
+      (format t #?"[${mt}/${mv}] [${st}/${sv}] [${lt}/${lv}] [Twolang/0.1]~%"))
     (finish-output)))
 
 (defun maybe-start-slynk ()
@@ -72,23 +79,59 @@ installed."
 
 (defun eval-line (line)
   (handler-bind ((error 'on-error))
-    (restart-case (eval-line* line)
+    (restart-case (or
+		   (maybe-eval-repl-command line)
+		   (eval-line* line))
       (skip-expression () nil))))
 
 (defun eval-line* (line)
-  (let* ((checked (tc! (parse (lex line))))
-	 (compiled (cc checked)))
-    (with-color (:black :effect :bright)
-      (format t "=> "))
-    (format t "~s" checked)
-    (format t "~s :: ~a~%~%"
+  (declare (special *repl-ctrl*))
+  (let ((lexed (lex line)))
+    (when (repl-opts-print-lex-p *repl-ctrl*)
+      (format t "~s~%" lexed))
+    (let ((parsed (parse lexed)))
+      (when (repl-opts-print-ast-p *repl-ctrl*)
+	(format t "~s~%" parsed))
+      (let* ((checked (tc! (parse (lex line))))
+	     (compiled (cc checked)))
+	(when (repl-opts-print-lisp-p *repl-ctrl*)
+	  (format t "~s~%" compiled))
+	(with-color (:black :effect :bright)
+	  (format t "=> "))
+	(format t "~s :: ~a~%~%"
 	    (funcall (compile nil `(lambda () ,compiled)))
-	    (type-hrepr (node-type checked)))))
+	    (type-hrepr (node-type checked)))))))
 
 (defun on-error (error)
-  (declare (special *trap-errors-p*))
   (if *debugger-hook*
       (invoke-debugger error)
       (progn
 	(format t "Error: ~a~%~%" error)
 	(invoke-restart (find-restart 'skip-expression)))))
+
+(defun maybe-eval-repl-command (line)
+  (declare (special *repl-ctrl*))
+  (let ((line (string-trim '(#\Space #\Tab #\Newline) line)))
+    (when (equal (uiop:first-char line) #\!)
+      (cond
+	((equal line "!lex+") (setf (repl-opts-print-lex-p *repl-ctrl*) t))
+	((equal line "!lex-") (setf (repl-opts-print-lex-p *repl-ctrl*) nil))
+	((equal line "!ast+") (setf (repl-opts-print-ast-p *repl-ctrl*) t))
+	((equal line "!ast-") (setf (repl-opts-print-ast-p *repl-ctrl*) nil))
+	((equal line "!lisp+") (setf (repl-opts-print-lisp-p *repl-ctrl*) t))
+	((equal line "!lisp-") (setf (repl-opts-print-lisp-p *repl-ctrl*) nil))
+	((equal (uiop:first-char line) #\!) (format t "[err] unknown command~%")))
+      (print-repl-ctrl)
+      t)))
+
+(defun print-repl-ctrl ()
+  (declare (special *repl-ctrl*))
+  (let ((lexp (repl-opts-print-lex-p *repl-ctrl*))
+	(astp (repl-opts-print-ast-p *repl-ctrl*))
+	(lispp (repl-opts-print-lisp-p *repl-ctrl*))))
+  (with-color (:black :effect :bright)
+    (format t "[lex~a] " (if (repl-opts-print-lex-p *repl-ctrl*) #\+ #\-))
+    (format t "[ast~a] " (if (repl-opts-print-ast-p *repl-ctrl*) #\+ #\-))
+    (format t "[lisp~a] " (if (repl-opts-print-lisp-p *repl-ctrl*) #\+ #\-))
+    (format t "~%~%"))
+  (finish-output))
